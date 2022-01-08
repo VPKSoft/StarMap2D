9ba414ce539625ec4b06927f373b849f391016dd
VPKSoft <vpksoft@vpksoft.net>
2022-01-08 17:57:00 +0200

    Add IAU rank.

--- StarMap2D.Calculations/Constellations/ConstellationNameIdentifier.cs
+++ StarMap2D.Calculations/Constellations/ConstellationNameIdentifier.cs
@@ -19,5 +19,13 @@ namespace StarMap2D.Calculations.Constellations
 
         /// <inheritdoc cref="IConstellationNameIdentifier.IdentifierValue"/>
         public ConstellationValue IdentifierValue { get; init; }
+
+        /// <summary>
+        /// Gets or sets the IAU rank of the constellation.
+        /// </summary>
+        /// <value>The IAU rank of the constellation.</value>
+        /// <remarks>See: https://en.wikipedia.org/wiki/IAU_designated_constellations_by_area</remarks>
+        // ReSharper disable once InconsistentNaming, The IAU is written in upper case.
+        public int IAURank { get; init; }
     }
 }
