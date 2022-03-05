156829f09c5c78ff781ca47948db7acba5f5ea0b
VPKSoft <vpksoft@vpksoft.net>
2022-03-05 20:10:59 +0200

    Remove ScottPlot - not looking good with GTK.

--- StarMap2D.EtoForms/MainForm.cs
+++ StarMap2D.EtoForms/MainForm.cs
@@ -24,15 +24,8 @@ SOFTWARE.
 */
 #endregion
 
-using System;
-using AASharp;
 using Eto.Drawing;
 using Eto.Forms;
-using ScottPlot;
-using ScottPlot.Eto;
-using ScottPlot.Plottable;
-using StarMap2D.Calculations.Extensions;
-using StarMap2D.Calculations.Helpers.Math;
 using StarMap2D.EtoForms.ApplicationSettings.SettingClasses;
 using StarMap2D.EtoForms.Forms;
 using StarMap2D.EtoForms.Forms.Dialogs;
@@ -69,7 +62,6 @@ namespace StarMap2D.EtoForms
                     {
                         Cells =
                         {
-                            sunMoonPlot,
                         }
                     },
                 },
@@ -113,122 +105,6 @@ namespace StarMap2D.EtoForms
 
             // create toolbar			
             ToolBar = new ToolBar { Items = { starMapCommand, new SeparatorToolItem(), settingsMenu } };
-
-            PlotSunMoon();
-            MouseMove += MainForm_MouseMove;
-
-            sunMoonPlot.Focus();
-        }
-
-        private void MainForm_MouseMove(object? sender, MouseEventArgs e)
-        {
-            SunMoonPlot_MouseMove(sender, e);
-        }
-
-        private Crosshair? sunCrossHair;
-        private Crosshair? moonCrossHair;
-
-        private ScatterPlot? sunPlot;
-        private ScatterPlot? moonPlot;
-
-        private readonly PlotView sunMoonPlot = new() { Width = 50, Height = 50, };
-
-        private void PlotSunMoon()
-        {
-            sunMoonPlot.MouseMove += SunMoonPlot_MouseMove;
-            sunMoonPlot.MouseDown += SunMoonPlot_MouseDown;
-
-            var xSunDoubles = new double[60 * 24];
-            var ySunDoubles = new double[60 * 24];
-
-            var xMoonDoubles = new double[60 * 24];
-            var yMoonDoubles = new double[60 * 24];
-
-            var dateTime = DateTime.UtcNow;
-
-            var offset = DateTimeOffset.Now.Offset.TotalHours;
-
-            dateTime =
-                new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 0, 0, 0, DateTimeKind.Utc).AddHours(-offset);
-
-            var date = dateTime.ToAASDate();
-
-            var latitude = Globals.Settings.Latitude;
-            var longitude = Globals.Settings.Longitude;
-
-            for (int i = 0; i < 60 * 24; i++)
-            {
-                date = date.AddSecondsFast(60);
-                var x = date.Hours(offset);
-                xSunDoubles[i] = x;
-                xMoonDoubles[i] = x;
-
-                var position = SolarSystemObjectPositions.GetObjectPosition(AASEllipticalObject.SUN, date, false, longitude, latitude);
-                ySunDoubles[i] = position.Y;
-
-                position = SolarSystemObjectPositions.GetMoonPosition(date, true, longitude, latitude);
-                yMoonDoubles[i] = position.Y;
-            }
-
-            sunPlot = sunMoonPlot.Plot.AddScatterLines(xSunDoubles, ySunDoubles);
-            sunPlot.XAxisIndex = 0;
-            sunPlot.YAxisIndex = 0;
-            sunMoonPlot.Plot.XAxis.Color(sunPlot.Color);
-            sunMoonPlot.Plot.YAxis.Color(sunPlot.Color);
-
-            moonPlot = sunMoonPlot.Plot.AddScatterLines(xMoonDoubles, yMoonDoubles);
-            moonPlot.XAxisIndex = 1;
-            moonPlot.YAxisIndex = 1;
-            sunMoonPlot.Plot.XAxis2.Color(moonPlot.Color);
-            sunMoonPlot.Plot.YAxis2.Color(moonPlot.Color);
-
-
-            sunCrossHair = sunMoonPlot.Plot.AddCrosshair(0, 0);
-            sunCrossHair.VerticalLine.PositionFormatter = hours => TimeSpan.FromHours(hours).ToString(@"hh\:mm\:ss");
-            sunCrossHair.Color = sunPlot.Color;
-
-            moonCrossHair = sunMoonPlot.Plot.AddCrosshair(0, 0);
-            moonCrossHair.VerticalLine.PositionFormatter = hours => TimeSpan.FromHours(hours).ToString(@"hh\:mm\:ss");
-            moonCrossHair.Color = moonPlot.Color;
-            moonCrossHair.HorizontalLine.PositionLabelOppositeAxis = true;
-            moonCrossHair.VerticalLine.PositionLabelOppositeAxis = true;
-
-            sunMoonPlot.Plot.XAxis.Label(UI.SunTime);
-            sunMoonPlot.Plot.YAxis.Label(UI.SunDegrees);
-
-            sunMoonPlot.Plot.XAxis2.Label(UI.MoonTime);
-            sunMoonPlot.Plot.YAxis2.Label(UI.MoonDegrees);
-
-            sunMoonPlot.Refresh();
-
-            sunMoonPlot.Plot.YAxis2.LockLimits();
-            sunMoonPlot.Plot.YAxis.LockLimits();
-            sunMoonPlot.Plot.XAxis.LockLimits();
-            sunMoonPlot.Plot.XAxis2.LockLimits();
-        }
-
-        private void SunMoonPlot_MouseDown(object? sender, MouseEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void SunMoonPlot_MouseMove(object? sender, MouseEventArgs e)
-        {
-            if (sunPlot != null && sunCrossHair != null && moonCrossHair != null && moonPlot != null)
-            {
-                var (mouseCoordinateX, _) = sunMoonPlot.GetMouseCoordinates();
-                var (pointX, pointY, _) = sunPlot.GetPointNearestX(mouseCoordinateX);
-
-                sunMoonPlot.GetMouseCoordinates();
-
-                sunCrossHair.X = pointX;
-                sunCrossHair.Y = pointY;
-                (pointX, pointY, _) = moonPlot.GetPointNearestX(mouseCoordinateX);
-                moonCrossHair.X = pointX;
-                moonCrossHair.Y = pointY;
-            }
-
-            sunMoonPlot.RefreshRequest(RenderType.HighQualityDelayed);
         }
     }
 }
