2335eaf352129e1d481124e8fbf8ce913b117f7e
VPKSoft <vpksoft@vpksoft.net>
2022-03-07 08:15:24 +0200

    Draft the time graph plot.

--- StarMap2D.EtoForms.Controls/TimeValuePlot.cs
+++ StarMap2D.EtoForms.Controls/TimeValuePlot.cs
@@ -69,10 +69,22 @@ namespace StarMap2D.EtoForms.Controls
             Invalidate();
         }
 
+        /// <summary>
+        /// Gets the axis minimum value for all axes specified in the <see cref="AxisData"/> property.
+        /// </summary>
+        /// <value>The axes minimum.</value>
         private float AxisMinimum => (float)AxisData.Min(f => f.MinimumValue);
 
+        /// <summary>
+        /// Gets the axis maximum value for all axes specified in the <see cref="AxisData"/> property.
+        /// </summary>
+        /// <value>The axes maximum.</value>
         private float AxisMaximum => (float)AxisData.Max(f => f.MaximumValue);
 
+        /// <summary>
+        /// Gets the axis absolute maximum value for all axes specified in the <see cref="AxisData"/> property.
+        /// </summary>
+        /// <value>The axes absolute absolute maximum.</value>
         private float AxisMinMax => Math.Max(Math.Abs(AxisMinimum), Math.Abs(AxisMaximum));
 
         private double CombinedDataLengthMinutes
@@ -85,15 +97,26 @@ namespace StarMap2D.EtoForms.Controls
             }
         }
 
-        // TODO::Document this!
         private void PlotAltitudeAzimuth_Paint(object? sender, PaintEventArgs e)
         {
+            DrawGraph(e.Graphics, e.ClipRectangle);
+        }
+
+        /// <summary>
+        /// Draws the graph(s) and axes to the specified graphics.
+        /// </summary>
+        /// <param name="graphics">The graphics to draw on to.</param>
+        /// <param name="drawArea">The drawing area rectangle.</param>
+        private void DrawGraph(Graphics graphics, RectangleF drawArea)
+        {
             using var backgroundBrush = new SolidBrush(base.BackgroundColor);
-            e.Graphics.FillRectangle(backgroundBrush, e.ClipRectangle);
+            graphics.FillRectangle(backgroundBrush, drawArea);
 
             var yCenter = (Height - AxisPadding.Vertical) / 2 + AxisPadding.Top;
             var multiplier = (Height - AxisPadding.Vertical) / 2f / Math.Max(Math.Abs(AxisMinimum), Math.Abs(AxisMaximum));
 
+            // If both values are positive, set the center to bottom and use a different multiplier.
+            // NOTE: Never tested.
             if (AxisMinimum > 0 && AxisMaximum > 0)
             {
                 yCenter = Height - AxisPadding.Bottom;
@@ -109,64 +132,74 @@ namespace StarMap2D.EtoForms.Controls
                     var y2 = yCenter - (float)axisData.YValues[i] * multiplier;
                     var x1 = i + AxisPadding.Left;
                     var x2 = x1 + 1;
-                    e.Graphics.DrawLine(axisData.PlotColor, new PointF(x1, y1), new PointF(x2, y2));
+                    graphics.DrawLine(axisData.PlotColor, new PointF(x1, y1), new PointF(x2, y2));
                 }
             }
 
-            // Draw the Y-axis markings.
+            // Draw the positive Y-axis markings.
             for (var i = 0.0; i < AxisMinMax; i += YLabelStepping)
             {
                 var y = yCenter - (float)i * multiplier;
-                e.Graphics.DrawLine(YAxisMarkerColor, new PointF(AxisPadding.Left - 10, y), new PointF(AxisPadding.Left, y));
+                graphics.DrawLine(YAxisMarkerColor, new PointF(AxisPadding.Left - 10, y), new PointF(AxisPadding.Left, y));
 
-                // TODO::Parameterize
-                var yLabelText = $"{i}°";
-                var size = e.Graphics.MeasureString(Font, yLabelText);
-                e.Graphics.DrawText(Font, YAxisMarkerColor, (AxisPadding.Left - size.Width) / 2f, y - size.Height / 2, yLabelText);
+                var yLabelText = string.Format(YLabelFormat, i);
+                var size = graphics.MeasureString(Font, yLabelText);
+                graphics.DrawText(Font, YAxisMarkerColor, (AxisPadding.Left - size.Width) / 2f, y - size.Height / 2, yLabelText);
             }
 
+            // Draw the negative Y-axis markings.
             for (var i = -YLabelStepping; i > -AxisMinMax; i -= YLabelStepping)
             {
                 var y = yCenter - (float)i * multiplier;
-                e.Graphics.DrawLine(YAxisMarkerColor, new PointF(AxisPadding.Left - 10, y), new PointF(AxisPadding.Left, y));
+                graphics.DrawLine(YAxisMarkerColor, new PointF(AxisPadding.Left - 10, y), new PointF(AxisPadding.Left, y));
 
-                var yLabelText = $"{i}°";
-                var size = e.Graphics.MeasureString(Font, yLabelText);
-                e.Graphics.DrawText(Font, YAxisMarkerColor, (AxisPadding.Left - size.Width) / 2f, y - size.Height / 2, yLabelText);
+                var yLabelText = string.Format(YLabelFormat, i);
+                var size = graphics.MeasureString(Font, yLabelText);
+                graphics.DrawText(Font, YAxisMarkerColor, (AxisPadding.Left - size.Width) / 2f, y - size.Height / 2, yLabelText);
             }
 
             // Draw the X-axis markings.
             for (var i = 0; i <= CombinedDataLengthMinutes; i += 60)
             {
-                var x = e.ClipRectangle.Left + AxisPadding.Left + (i / CombinedDataLengthMinutes) * (Width - AxisPadding.Horizontal);
-                e.Graphics.DrawLine(XAxisMarkerColor,
-                    new PointF((float)x, e.ClipRectangle.Bottom - AxisPadding.Bottom),
-                    new PointF((float)x, e.ClipRectangle.Bottom - AxisPadding.Bottom + 10));
+                var timeSpan = TimeSpan.FromMinutes(i);
+                if ((timeSpan.Hours % HourLabelFrequency) != 0)
+                {
+                    // Skip according to the parametrization.
+                    continue;
+                }
 
-                // TODO::Parameterize
-                var xLabelText = TimeSpan.FromMinutes(i).ToString(@"hh");
-                var size = e.Graphics.MeasureString(Font, xLabelText);
-                e.Graphics.DrawText(Font, YAxisMarkerColor, (float)x - size.Width / 2f, e.ClipRectangle.Bottom - 10 - size.Height, xLabelText);
+                var x = drawArea.Left + AxisPadding.Left + (i / CombinedDataLengthMinutes) * (Width - AxisPadding.Horizontal);
+                graphics.DrawLine(XAxisMarkerColor,
+                    new PointF((float)x, drawArea.Bottom - AxisPadding.Bottom),
+                    new PointF((float)x, drawArea.Bottom - AxisPadding.Bottom + 10));
+
+                var xLabelText = timeSpan.ToString(TimeLabelFormat);
+                var size = graphics.MeasureString(Font, xLabelText);
+                graphics.DrawText(Font, YAxisMarkerColor, (float)x - size.Width / 2f, drawArea.Bottom - 10 - size.Height, xLabelText);
             }
 
-            var topLeft = new PointF(e.ClipRectangle.Left + AxisPadding.Left, e.ClipRectangle.Top);
-            var bottomLeft = new PointF(e.ClipRectangle.Left + AxisPadding.Left, e.ClipRectangle.Bottom);
-            e.Graphics.DrawLine(AxisLineColor, topLeft, bottomLeft);
+            // Draw the X-axis line.
+            var topLeft = new PointF(drawArea.Left + AxisPadding.Left, drawArea.Top);
+            var bottomLeft = new PointF(drawArea.Left + AxisPadding.Left, drawArea.Bottom);
+            graphics.DrawLine(AxisLineColor, topLeft, bottomLeft);
 
+            // Draw the zero marker line.
             using var pen = new Pen(ZeroLineColor) { DashStyle = new DashStyle(10, 5) };
-            bottomLeft = new PointF(e.ClipRectangle.Left + AxisPadding.Left, yCenter);
-            var bottomRight = new PointF(e.ClipRectangle.Right, yCenter);
-            e.Graphics.DrawLine(pen, bottomLeft, bottomRight);
+            bottomLeft = new PointF(drawArea.Left + AxisPadding.Left, yCenter);
+            var bottomRight = new PointF(drawArea.Right, yCenter);
+            graphics.DrawLine(pen, bottomLeft, bottomRight);
 
-            bottomLeft = new PointF(e.ClipRectangle.Left, e.ClipRectangle.Bottom - AxisPadding.Bottom);
-            bottomRight = new PointF(e.ClipRectangle.Right, e.ClipRectangle.Bottom - AxisPadding.Bottom);
-            e.Graphics.DrawLine(AxisLineColor, bottomLeft, bottomRight);
+            // Draw the Y-axis line.
+            bottomLeft = new PointF(drawArea.Left, drawArea.Bottom - AxisPadding.Bottom);
+            bottomRight = new PointF(drawArea.Right, drawArea.Bottom - AxisPadding.Bottom);
+            graphics.DrawLine(AxisLineColor, bottomLeft, bottomRight);
 
-            topLeft = new PointF(e.ClipRectangle.Left + AxisPadding.Left, e.ClipRectangle.Top + AxisPadding.Top);
-            var topRight = new PointF(e.ClipRectangle.Right - AxisPadding.Right, e.ClipRectangle.Top + AxisPadding.Top);
-            bottomRight = new PointF(e.ClipRectangle.Right - AxisPadding.Right, e.ClipRectangle.Bottom - AxisPadding.Bottom);
-            e.Graphics.DrawLine(AxisLineColor, topLeft, topRight);
-            e.Graphics.DrawLine(AxisLineColor, topRight, bottomRight);
+            // Draw the to and right area limit lines.
+            topLeft = new PointF(drawArea.Left + AxisPadding.Left, drawArea.Top + AxisPadding.Top);
+            var topRight = new PointF(drawArea.Right - AxisPadding.Right, drawArea.Top + AxisPadding.Top);
+            bottomRight = new PointF(drawArea.Right - AxisPadding.Right, drawArea.Bottom - AxisPadding.Bottom);
+            graphics.DrawLine(AxisLineColor, topLeft, topRight);
+            graphics.DrawLine(AxisLineColor, topRight, bottomRight);
         }
 
         #region PublicProperties
@@ -174,7 +207,7 @@ namespace StarMap2D.EtoForms.Controls
         /// Gets or sets the color for the background of the control
         /// </summary>
         /// <value>The color of the background.</value>
-        /// <remarks>Note that on some platforms (e.g. Mac), setting the background color of a control can change the performance
+        /// <remarks>Note that on some platforms (g. Mac), setting the background color of a control can change the performance
         /// characteristics of the control and its children, since it must enable layers to do so.</remarks>
         public new Color BackgroundColor
         {
@@ -349,10 +382,56 @@ namespace StarMap2D.EtoForms.Controls
             }
         }
 
+        private string yLabelFormat = "{0:F0}°";
+
         /// <summary>
-        /// Gets the axis data collection.
+        /// Gets or sets the Y-axis label format.
         /// </summary>
-        /// <value>The axis data collection.</value>
+        /// <value>The Y-axis label format.</value>
+        public string YLabelFormat
+        {
+            get => yLabelFormat;
+
+            set
+            {
+                if (yLabelFormat != value)
+                {
+                    yLabelFormat = value;
+                    Invalidate();
+                }
+            }
+        }
+
+        private string timeLabelFormat = "hh";
+
+        /// <summary>
+        /// Gets or sets the time label format.
+        /// </summary>
+        /// <value>The time label format.</value>
+        /// <remarks>
+        /// The format is for method <see cref="TimeSpan.ToString(string)"/> method. For more info see: <para/>
+        /// * https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-timespan-format-strings<para/>
+        /// * https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-timespan-format-strings<para/>
+        /// E.g. "hh\:mm\:ss".
+        /// </remarks>
+        public string TimeLabelFormat
+        {
+            get => timeLabelFormat;
+
+            set
+            {
+                if (value != timeLabelFormat)
+                {
+                    timeLabelFormat = value;
+                    Invalidate();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the axes data collection.
+        /// </summary>
+        /// <value>The axes data collection.</value>
         public ObservableCollection<AxisData> AxisData { get; } = new();
         #endregion
     }
